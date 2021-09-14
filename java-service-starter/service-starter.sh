7ac1830bc2864204b13092f7195c76183972088e
Alex <sagorski.entwicklung@gmail.com>
2021-09-14 17:06:48 +0200

    Added files

---
+++ java-service-starter/service-starter.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+SERVICE_NAME=java-service-starter
+PATH_TO_JAR=/usr/local/bin/java-service-starter/servicetester.jar
+PID_PATH_NAME=/tmp/testservice-pid
+
+case $1 in
+    start)
+        echo "Starting $SERVICE_NAME ..."
+        if [ ! -f $PID_PATH_NAME ]; then
+            # nohup /var/www/html/java/jdk-16.0.1+4/bin/java -jar $PATH_TO_JAR >> /usr/local/bin/javatest/output.out 2>&1&
+            nohup /path/to/java -jar $PATH_TO_JAR >> /usr/local/bin/java-service-starter/output.out 2>&1&
+            echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is already running ..."
+        fi
+    ;;
+    stop)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stoping ..."
+            kill $PID;
+            echo "$SERVICE_NAME stopped ..."
+            rm $PID_PATH_NAME
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+    restart)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stopping ...";
+            kill $PID;
+            echo "$SERVICE_NAME stopped ...";
+            rm $PID_PATH_NAME
+            echo "$SERVICE_NAME starting ..."
+            #  nohup /var/www/html/java/jdk-16.0.1+4/bin/java -jar $PATH_TO_JAR >> /usr/local/bin/javatest/output.out 2>&1&
+            nohup /path/to/java -jar $PATH_TO_JAR >> /usr/local/bin/java-service-starter/output.out 2>&1&
+            echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+esac 
+
